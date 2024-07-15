0d5bbc79ac65cf0927953fdb6dd7565dc9e1a6b9
1876070677 <fixerpremium@gmail.com>
2024-07-15 14:38:25 +0900

    Modify: security CORS off

--- src/main/java/cuk/api/Config/Security/TrinitySecurityConfig.java
+++ src/main/java/cuk/api/Config/Security/TrinitySecurityConfig.java
@@ -21,6 +21,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -40,7 +43,7 @@ public class TrinitySecurityConfig {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
-                .cors().disable()
+                .cors().configurationSource(corsConfigurationSource()).and()
                 .headers().disable()
                 .httpBasic().disable()
                 .formLogin().disable()
@@ -98,5 +101,16 @@ public class TrinitySecurityConfig {
     @Bean
     public JSONAccessDeniedHandler jsonAccessDeniedHandler() {
         return new JSONAccessDeniedHandler(objectMapper);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }

491eb80b4ada883e4cfc9f7a28ba79c53ddec743
1876070677 <fixerpremium@gmail.com>
2024-07-22 16:14:23 +0900

    Modify: Cookie Domain

--- src/main/java/cuk/api/Config/Session/RedisSessionConfig.java
+++ src/main/java/cuk/api/Config/Session/RedisSessionConfig.java
@@ -46,7 +46,6 @@ public class RedisSessionConfig {
         cookieSerializer.setCookieName("SESSION");
         cookieSerializer.setUseSecureCookie(true);
         cookieSerializer.setUseHttpOnlyCookie(true);
-        cookieSerializer.setDomainName(".dobby.kr");
         cookieSerializer.setSameSite("None");
 
         return cookieSerializer;

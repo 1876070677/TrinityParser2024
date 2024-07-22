278b3f8c05820c0d69df57f19604bd2a4853e988
1876070677 <fixerpremium@gmail.com>
2024-07-22 14:56:55 +0900

    Modify: Same-site

--- src/main/java/cuk/api/Config/Session/RedisSessionConfig.java
+++ src/main/java/cuk/api/Config/Session/RedisSessionConfig.java
@@ -46,7 +46,7 @@ public class RedisSessionConfig {
         cookieSerializer.setCookieName("SESSION");
         cookieSerializer.setUseSecureCookie(true);
         cookieSerializer.setUseHttpOnlyCookie(true);
-        cookieSerializer.setSameSite("Lax");
+        cookieSerializer.setSameSite("None");
 
         return cookieSerializer;
     }

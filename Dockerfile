80fb5ea82eb7fbee7c9cb24ce05105a43cd6dff6
1876070677 <fixerpremium@gmail.com>
2024-08-05 17:24:39 +0900

    Delete: JMX

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ RUN ["rm", "/etc/localtime"]
 RUN ["ln", "-sf", "/usr/share/zoneinfo/Asia/Seoul", "/etc/localtime"]
 
 COPY ./target/TrinityParser-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/ROOT.war
-COPY ./TomcatENV/setenv.sh /usr/local/tomcat/bin/
 
 #start tomcat
 CMD ["/usr/local/tomcat/bin/catalina.sh", "run"]
--- TomcatENV/setenv.sh
+++
@@ -1,7 +0,0 @@
-# JMX 설정 추가
-export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true"
-export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
-export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
-export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=9000"
-export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.rmi.port=9000"
-export CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=localhost"

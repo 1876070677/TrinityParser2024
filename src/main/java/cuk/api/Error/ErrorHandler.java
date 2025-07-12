a52689a6a02bbc8782337c62256883ccd6a7a03d
1876070677 <kdl5001@catholic.ac.kr>
2025-07-12 23:21:33 +0900

    (*) 404에 접근 시 초기 페이지로 forwarding
    - 존재하지 않는 url 접근 시 초기 페이지로 접근되도록 구현.

--- src/main/java/cuk/api/Error/ErrorHandler.java
+++ src/main/java/cuk/api/Error/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ErrorHandler {
@@ -14,5 +15,10 @@ public class ErrorHandler {
         resp.setStatus(HttpStatus.BAD_REQUEST);
         resp.setMessage(e.getMessage());
         return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String fallback() {
+        return "forward:/fe/index.html";
     }
 }

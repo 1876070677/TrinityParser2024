c9d80a0776e551a9f9b8ddec87daf0fb8d236c03
1876070677 <kdl5001@catholic.ac.kr>
2025-08-02 23:44:19 +0900

    (*) vite에서 production에서는 proxy랑 https 사용안하도록 설정
    - dev모드에서만 필요한 설정들을 빌드시 사용하지 않도록 수정.

--- frontend-renew/vite.config.ts
+++ frontend-renew/vite.config.ts
@@ -2,27 +2,32 @@ import { defineConfig, ConfigEnv, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
     base: '/fe',
     plugins: [react()],
     server: {
-      https: {
-        key: fs.readFileSync('./mkcert/localhost+1-key.pem'),
-        cert: fs.readFileSync('./mkcert/localhost+1.pem'),
-      },
-      proxy: {
-        '/manage': {
-          target: 'https://trinity.dobby.kr',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path,  // 경로 그대로 전달
+      ...(isDev && {
+        https: {
+          key: fs.readFileSync('./mkcert/localhost+1-key.pem'),
+          cert: fs.readFileSync('./mkcert/localhost+1.pem'),
         },
-        '/trinity': {
-          target: 'https://trinity.dobby.kr',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path,  // 경로 그대로 전달
+        proxy: {
+          '/manage': {
+            target: 'https://trinity.dobby.kr',
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path,  // 경로 그대로 전달
+          },
+          '/trinity': {
+            target: 'https://trinity.dobby.kr',
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path,  // 경로 그대로 전달
+          }
         }
-      }
-    },
-  });
+      })
+    }
+  }
+});

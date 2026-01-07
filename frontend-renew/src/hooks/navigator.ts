d956b4d8d13effb06a9a530c7e55a0d2cba6f8ed
1876070677 <kdl5001@catholic.ac.kr>
2026-01-07 21:44:11 +0900

    대규모 업데이트
    - FE 및 BE의 서브모듈화.
    - 기존 parser들의 폴더 이동

--- frontend-renew/src/hooks/navigator.ts
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from 'react-router-dom';
-
-export const useMovePage = () => {
-    const navigate = useNavigate();
-
-    return (page: string) => {
-        navigate(page);
-    };
-};
--- frontend-renew/src/types/Main.ts
+++
@@ -1,1 +0,0 @@
-export type Mode = 'sbjtInq' | 'grade' | 'board' | 'faq';
